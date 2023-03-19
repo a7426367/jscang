a83f899f17883731d591fb259fbc217e2d903c35
a7426367 <62828920+a7426367@users.noreply.github.com>
2023-03-19 21:20:37 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+window.g = {
+  application: '通用B/S版OMC-Debug',
+  curFileVersionID: 2,
+  curBusinessSceneID: 1,
+  genericUrl: 'http://omc.kongtrolink.com:23232', // 配置服务器地址
+  //genericUrl: 'http://omc.kongtrolink.com:32805', // 配置服务器地址
+  // genericUrl: 'http://localhost:32705', // 配置本地服务器地址
+  compilerServer: 'http://omc.kongtrolink.com:32709',
+  updateServer: 'http://omc.kongtrolink.com:32703',
+  updateServerDownload: 'http://omc.kongtrolink.com:32704'
+}
